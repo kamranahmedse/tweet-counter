155ca2c054f880b9a0e116ab157caa49994ad216
Kamran Ahmed <kamranahmed.se@gmail.com>
2017-11-09 01:09:30 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Twitter 140
+> Chrome Extension - Limit your tweets to 140 characters
+
+## Installation
+
+Install it from the [Chrome web store]() or [install it manually by cloning](https://superuser.com/questions/247651/how-does-one-install-an-extension-for-chrome-browser-from-the-local-file-system/247654#247654)
+
+Voila! you have got your 140 character limit back
+
+## Feedback
+Reach out to me directly at kamranahmed.se@gmail.com or [![Twitter URL](https://img.shields.io/twitter/url/https/twitter.com/kamranahmedse.svg?style=social&label=Follow%20%40kamranahmedse)](https://twitter.com/kamranahmedse)
+
+## License
+MIT © [Kamran Ahmed](https://kamranahmed.info)
+
+
---
+++ counter.js
@@ -0,0 +1,28 @@
+const tweetBoxSelector = '.RichEditor-container';
+const counterSelector = '.js-countdown-counter';
+const editorSelector = '.tweet-box.rich-editor';
+
+const populateCount = (target) => {
+    const $target = $(target);
+    const $counter = $target.closest(tweetBoxSelector).find(counterSelector);
+
+    let text = $target.text();
+
+    $counter.html(twttr.txt.getTweetLength(text));
+};
+
+// For each of the tweet boxes on page, observe the changes
+$(tweetBoxSelector).each(function () {
+    const target = $(this).find(editorSelector).get(0);
+
+    // Observer to observe the tweet boxes
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach((mutation) => populateCount(target));
+    });
+
+    observer.observe(target, {
+        attributes: true,
+        childList: true,
+        characterData: true,
+    });
+});
