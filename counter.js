e3ec05010cc0c5720730d09f2dc8bee2c6782c29
Alexander Ein <435658+anein@users.noreply.github.com>
2017-11-23 05:59:03 +0300

    chore: use vanilla JS selectors instead of jQuery

--- counter.js
+++ counter.js
@@ -1,29 +1,26 @@
-const tweetBoxSelector = '.RichEditor-container';
-const counterSelector = '.js-countdown-counter';
+const tweetBoxSelector = 'RichEditor-container';
+const counterSelector = 'js-countdown-counter';
 const editorSelector = '.tweet-box.rich-editor';
 
-// Populates the tweet character count
-const populateCount = (target) => {
-    const $target = $(target);
-    const $counter = $target.closest(tweetBoxSelector).find(counterSelector);
-
-    let text = $target.text();
-
-    $counter.html(twttr.txt.getTweetLength(text));
-};
-
 // For each of the tweet boxes on page, observe the changes
-$(tweetBoxSelector).each(function () {
-    const target = $(this).find(editorSelector).get(0);
+Array.from(document.getElementsByClassName(tweetBoxSelector)).forEach((item) => {
 
-    // Observer to observe the tweet boxes
-    const observer = new MutationObserver(mutations => {
-        mutations.forEach((mutation) => populateCount(target));
-    });
+  const target = item.querySelector(editorSelector);
+  const counter = item.getElementsByClassName(counterSelector)[0];
 
-    observer.observe(target, {
-        attributes: true,
-        childList: true,
-        characterData: true,
-    });
+  // Observer to observe the tweet boxes
+  const observer = new MutationObserver(() => {
+
+    const text = target.textContent;
+
+    counter.textContent = twttr.txt.getTweetLength(text);
+
+  });
+
+  observer.observe(target, {
+    attributes: true,
+    childList: true,
+    characterData: true,
+  });
+
 });
